0c3bb5922a45b2cce76664f30381c60772fb7117
jaspinderpawar <jaspinder@founderandlightning.com>
2024-07-31 20:36:01 +0530

    Return data

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/users', function (req, res) {
+    res.status(200).json({
+      success: true,
+      data: [{
+        id: 1,
+        name: 'John Doe',
+        email: 'john@yopmail.com'
+      }, {
+        id: 2,
+        name: 'Rohan Doe',
+        email: 'rohan@yopmail.com'
+      }]
+    });
 });
 app.listen(5000, function () {
   console.log('Example app listening on port 3000!');
